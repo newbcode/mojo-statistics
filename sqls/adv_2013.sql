045b06f6247c58575f5477019cdae1bbc528db47
YunChang Kang <codenewb@gmail.com>
2014-01-20 23:44:36 +0900

    morris add

---
+++ sqls/adv_2013.sql
@@ -0,0 +1,12 @@
+CREATE TABLE `adv_2013` (
+  `id`          INT(11) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `author`      VARCHAR(512)      NOT NULL DEFAULT '',
+  `title`       VARCHAR(512)      NOT NULL,
+  `url`         TEXT             NOT NULL,
+  `abst`        TEXT             NOT NULL,
+  `likesum`     INT(2)           NOT NULL,
+  `year`        INT(2)           NOT NULL,
+  `wdate`       TIMESTAMP        NOT NULL DEFAULT CURRENT_TIMESTAMP,
+ 
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
